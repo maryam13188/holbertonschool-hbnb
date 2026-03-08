6c5924fb6767ceffcd8d7cf8ed45b87aa7320baf
aasiria24 <amaalmoasiri@gmail.com>
2026-03-08 11:41:01 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """HBnB Application Factory - Part 3 (Task 0)"""
-
+import os
 from flask import Flask
 from flask_restx import Api
-
 from config import DevelopmentConfig
 from app.extensions import db, bcrypt, jwt
 
-
 def create_app(config_class=DevelopmentConfig):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_path=os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'instance'
+    ))
     app.config.from_object(config_class)
-
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
-
     api = Api(
         app,
         version="1.0",
@@ -23,17 +21,14 @@ def create_app(config_class=DevelopmentConfig):
         description="HBnB Application API",
         doc="/api/v1/",
     )
-
-    from app.api.v1.auth import api as auth_ns    #Amaal
+    from app.api.v1.auth import api as auth_ns
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
-
-    api.add_namespace(auth_ns, path="/api/v1/auth")    #Amaal
+    api.add_namespace(auth_ns, path="/api/v1/auth")
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
-
     return app
