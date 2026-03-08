94593d1171364bdbfa98f367f2dbe1179e343a5e
aasiria24 <amaalmoasiri@gmail.com>
2026-03-08 08:59:22 +0000

    Fix DB path and JWT auth - Tasks 2,3,4,8 working

--- part3/run.py
+++ part3/run.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """HBnB Part 3 entry point"""
-
 from app import create_app
 from config import DevelopmentConfig
-
 app = create_app(DevelopmentConfig)
-
+print("DB URI:", app.config['SQLALCHEMY_DATABASE_URI'])
+print("instance_path:", app.instance_path)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
